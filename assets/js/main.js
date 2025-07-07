9e52743eea088f6c4d3898e2a25aa3f49f13db8b
Taposh paul <taposhp05@gmail.com>
2025-07-07 13:12:12 +0600

    add image reveal animetion on scroll used gsap animetion

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,17 +9,77 @@ $(document).ready(function () {
         dropdawn.slideToggle()
     })
 
-    // slider 
-    const swiper = new Swiper(".swiper", {
-        slidesPerView: "auto",
+    // gsap animation
+    gsap.registerPlugin(ScrollTrigger);
+
+    // images animation
+
+    const leftimages = gsap.utils.toArray(".img__slide--left-to-right");
+    const rightimages = gsap.utils.toArray(".img__slide--right-to-left");
+
+    leftimages.forEach((image) => {
+        gsap.fromTo(image,
+            {
+                x: "-5%",
+                clipPath: "inset(0 100% 0 0)",
+                opacity: 0,
+            },
+            {
+                x: "0",
+                clipPath: "inset(0 0% 0 0)",
+                opacity: 1,
+                duration: 1,
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: image,
+                    start: "top 80%",
+                    end: "bottom 20%",
+                    toggleActions: "play pause resume none",
+                    once: false
+                }
+            }
+        )
+    });
+    rightimages.forEach((image) => {
+        gsap.fromTo(image,
+            {
+                x: "5%",
+                clipPath: "inset(0 0 0 100%)",
+                opacity: 0,
+            },
+            {
+                x: "0",
+                clipPath: "inset(0 0 0 0%)",
+                opacity: 1,
+                duration: 1,
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: image,
+                    start: "top 80%",
+                    end: "bottom 20%",
+                    toggleActions: "play pause resume none",
+                    once: false
+                }
+            }
+        )
+    })
+
+    // swiper slider 
+    const swiper = new Swiper(".runing__swiper", {
         loop: true,
-        speed: 4000, // Slow speed for smooth continuous scroll
+        slidesPerView: 'auto',
         autoplay: {
-            delay: 0, // No delay between transitions
+            delay: 0,
             disableOnInteraction: false,
         },
+        speed: 4000,
         freeMode: true,
         freeModeMomentum: false,
+        loopedSlides: 6,
+        allowTouchMove: false,
+        slidesPerGroup: 1,
+        loopAdditionalSlides: 6,
+        cssMode: false,
     })
     // slider 2
     const swiper2 = new Swiper(".testimonial__slider", {
